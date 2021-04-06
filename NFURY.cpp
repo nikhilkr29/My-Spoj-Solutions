d6e4896db43dd784c57a9b0345d56afe19bdb58d
Nikhil Kumar <58744718+nikhilkr29@users.noreply.github.com>
2021-04-06 18:46:37 +0530

    Update NFURY.cpp

--- NFURY.cpp
+++ NFURY.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define  vi vector<long long int>
 #define  set1 set<long long int>
 #define  pb push_back
+#define  pob pop_back
 #define  map1 map<long long int,long long int>
 #define  ll long long int
 #define  mp make_pair
@@ -15,53 +16,69 @@ using namespace std;
 #define  mod 1000000007
 #define  pi  3.1415926535897932384626433832
 #define  ins insert
+#define  what_is(x) cerr << #x << " is " << x << endl;
 #define  w(t) long long int t;cin>>t;while(t--)
- 
-ll a[10005];
- 
+
+ll a[10000];
+
+//Precomputing for all values from 1 to 10000
+void precomp() {
+
+
+  //Initializing with a very large number
+  for (ll i = 1; i <= 1000; i++) {
+    a[i] = inf;
+  }
+
+  //Base Cases
+
+  a[0] = 0;
+  a[1] = 1;
+  a[2] = 2;
+  a[3] = 3;
+
+
+  //For all the perfect square from 1 to 1000 value will be 1
+  for (ll i = 1; i <= 35; i++) {
+    a[i * i] = 1;
+  }
+
+  //Starting from 4
+  for (ll i = 4; i <= 1000; i++) {
+    //If it is a perfect square then simply continue
+    if (a[i] == 1) {
+      continue;
+    }
+    //Otherwise for all value less than i find the min possible ans
+    for (ll j = 1; j < i; j++) {
+      //Either a[i] is min or a[j]+a[i-j] is min
+      //Time Complexity is O(N^2) O(10^6) very much doable in 1 sec
+      a[i] = min(a[i], a[j] + a[i - j]);
+    }
+  }
+
+}
+
 void solve() {
- 
-	ll n;
-	cin >> n;
-	cout << a[n] << "\n";
- 
- 
- 
+
+  ll n;
+  cin >> n;
+  cout << a[n] << "\n";
+
+
 }
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 #ifndef ONLINE_JUDGE
-	freopen("inp.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
+  freopen("inp.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
 #endif
- 
-	memset(a, 0, sizeof(a));
-	a[1] = 1;
-	a[2] = 2;
-	a[3] = 3;
-	ll co = 0, id = 0;
-	for (ll i = 2; i <= 35; i++) {
-		a[i * i] = 1;
-	}
- 
-	for (ll i = 4; i <= 1000; i++) {
-		if (a[i] == 1) {
-			id = i;
-		}
-		else {
-			a[i] = min(1 + a[i - 1], 1 + a[i - id]);
-			for (ll j = 1; j < i; j++) {
-				a[i] = min(a[i], a[j] + a[i - j]);
-			}
- 
-		}
-	}
- 
- 
- 
-	w(t) {
-		solve();
-	}
-	return 0;
-} 
+
+  precomp();
+
+  w(t) {
+    solve();
+  }
+  return 0;
+}
