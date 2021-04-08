a09ebabfca7ec0f42671bd922801bbc6d593f727
Nikhil Kumar <58744718+nikhilkr29@users.noreply.github.com>
2021-04-08 18:03:04 +0530

    Update EDIST.cpp

--- EDIST.cpp
+++ EDIST.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define  vi vector<long long int>
 #define  set1 set<long long int>
 #define  pb push_back
+#define  pob pop_back
 #define  map1 map<long long int,long long int>
 #define  ll long long int
 #define  mp make_pair
@@ -15,66 +16,64 @@ using namespace std;
 #define  mod 1000000007
 #define  pi  3.1415926535897932384626433832
 #define  ins insert
+#define  what_is(x) cerr << #x << " is " << x << endl;
 #define  w(t) long long int t;cin>>t;while(t--)
- 
+
 ll dp[2005][2005];
-ll n, m;
- 
-ll solve1(string &s, string &p, ll i, ll j) {
- 
-	if (i == n) {
-		return m - j;
-	}
-	if (j == m) {
-		return n - i;
-	}
- 
- 
- 
-	if (dp[i][j] != -1) {
-		return dp[i][j];
-	}
- 
-	ll ans;
-	if (s[i] == p[j]) {
-		ans = solve1(s, p, i + 1, j + 1);
-	}
- 
-	else {
-		//Add
-		ll op1 = 1 + solve1(s, p, i, j + 1);
-		//Replace
-		ll op2 = 1 + solve1(s, p, i + 1, j + 1);
-		//Delete
-		ll op3 = 1 + solve1(s, p, i + 1, j);
-		ans = min({op1, op2, op3});
-	}
- 
- 
- 
-	return dp[i][j] = ans;
- 
+
+ll solve(string &s, string &p, ll id1, ll id2) {
+
+  ll ans = 0;
+  //Base Case
+  if (id1 == s.length() && id2 == p.length()) {
+    return 0;
+  }
+  else if (id1 == s.length()) {
+    return p.length() - id2;
+  }
+  else if (id2 == p.length()) {
+    return s.length() - id1;
+  }
+
+  if (dp[id1][id2] != -1) {
+    return dp[id1][id2];
+  }
+
+  if (s[id1] == p[id2]) {
+    ans = solve(s, p, id1 + 1, id2 + 1);
+  }
+  else {
+    //Insert
+    ll op1 = 1 + solve(s, p, id1, id2 + 1);
+    //Delete
+    ll op2 = 1 + solve(s, p, id1 + 1, id2);
+    //Replace
+    ll op3 = 1 + solve(s, p, id1 + 1, id2 + 1);
+    ans = min({op1, op2, op3});
+  }
+
+  return dp[id1][id2] = ans;
 }
- 
-void solve() {
- 
-	string s, p;
-	cin >> s >> p;
-	n = s.length();
-	m = p.length();
-	memset(dp, -1, sizeof(dp));
- 
-	cout << solve1(s, p, 0, 0) << "\n";
+
+void nk() {
+
+  string s, p;
+  cin >> s >> p;
+  memset(dp, -1, sizeof(dp));
+  cout << solve(s, p, 0, 0) << "\n";
+
+
+
 }
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 #ifndef ONLINE_JUDGE
-	freopen("inp.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
+  freopen("inp.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
 #endif
-	w(t) {
-		solve();
-	}
-	return 0;
-} 
+  w(t) {
+    nk();
+  }
+  return 0;
+}
